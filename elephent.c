5ad1c1bae10c8d732d5148859cba1a803b9e1be1
samdani91 <samdanimozumder91@gmail.com>
2022-12-08 08:48:30 +0600

    first commit

--- elephent.c
+++ elephent.c
@@ -9,18 +9,7 @@ int main()
     if(x%5==0){
         printf("%d",x/5);
     }else{
-        int a=x/5;
-        int b=x%5;
-        if(b==4) printf("%d",a+1);
-        if(b==3) printf()
-        int c=b/4;
-        int d=x%4;
-        int e=d/3;
-        int f=d%3;
-        int g=f/2;
-        int h=f%2;
-        int total=a+c+e+g+h;
-        printf("%d",total);
+        printf("%d",(x/5)+1);
     }
 
     return 0;
