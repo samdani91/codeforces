ba5390eec43ee19a15c9229784f6c41e0fd387db
samdani91 <samdanimozumder91@gmail.com>
2023-01-27 20:43:39 +0600

    bug

--- squarestring.cpp
+++ squarestring.cpp
@@ -11,19 +11,19 @@ int main()
         j=0;
     cin>>s1;
     int length=s1.size();
-    if(length%2!=0) cout<<"NO\n";
+    if(length%2!=0) cout<<"NO"<<endl;
     else{
-        for(i=length/2;i<length;i++){
+        for(i=length/2;s1[i]!='\0';i++){
             s2[j]=s1[i];
             s1[i]='\0';
             j++;
         }
         s2[j]='\0';
-        cout<<s1<<endl;
-        cout<<s2<<endl;
+        //cout<<s1<<endl;
+        //cout<<s2<<endl;
         int check=s1.compare(s2);
-        if(check==0) cout<<"YES\n";
-        else cout<<"NO\n";
+        if(check==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
 
     }
     }
