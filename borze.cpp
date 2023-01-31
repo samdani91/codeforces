a2b82d3d56479595407e912159e80c1b010de0e3
samdani91 <samdanimozumder91@gmail.com>
2023-01-31 12:27:55 +0600

    first

---
+++ borze.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s,s2;
+	cin>>s;
+
+	int length=s.size();
+	for(int i=0;i<length;i++){
+		if(s[i]=='.') cout<<"0";
+		else if(s[i]=='-' && s[i+1]=='-') {
+			cout<<"2";
+			i++;
+		}
+		else if(s[i]=='-' && s[i+1]=='.') {
+			cout<<"1";
+			i++;
+		}
+		
+		else if(s[i]=='.') cout<<"0";
+	}
+	
+}
