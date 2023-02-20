92811738161f23f62081b444d4aeb4594488056c
samdani91 <samdanimozumder91@gmail.com>
2023-02-20 22:44:54 +0600

    first

---
+++ A_New_Year_and_Hurry.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<<" "<<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int n,k;
+    cin>>n>>k;
+
+    int count=0,problem_time=0;
+    int total_time=240;
+    int rem_time=total_time-k;
+
+    for(int i=1;i<=n;i++){
+        problem_time+=5*i;
+        if(problem_time>rem_time) break;
+        count++;
+    }
+    cout<<count;
+    return 0;
+}
