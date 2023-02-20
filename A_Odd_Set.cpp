20f525ea139d6c310f13bec953dbc29322a9f855
samdani91 <samdanimozumder91@gmail.com>
2023-02-20 23:35:00 +0600

    first

---
+++ A_Odd_Set.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<<" "<<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int n,t;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int arr[2*n];
+        int odd=0,even=0;
+        for(int i=0;i<2*n;i++) {
+            cin>>arr[i];
+            if(arr[i]%2==0) even++;
+            else odd++;
+        }
+
+        if(n==1){
+            int sum=arr[0]+arr[1];
+            if(sum%2!=0) cout<<"Yes"<<endl;
+            else cout<<"No"<<endl;
+        }else{
+            if(odd==even) cout<<"Yes"<<endl;
+            else cout<<"No"<<endl;
+        }
+    }
+
+
+    return 0;
+}
