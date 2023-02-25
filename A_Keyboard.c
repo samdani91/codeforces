d7a8d518158e916d10100cf6fa9a991cc04a77bb
samdani91 <samdanimozumder91@gmail.com>
2023-02-25 22:56:53 +0600

    not done

---
+++ A_Keyboard.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc,char *argv[])
+{
+
+    char ch, str[101],str2[101],str3[101];
+    str[101]="qwertyuiopasdfghjkl;zxcvbnm,./";
+    scanf("%c/n",&ch);
+    scanf("%s",str2);
+
+    return 0;
+}
