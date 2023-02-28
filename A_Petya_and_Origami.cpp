f4d3fb976b4966451cc5634e5e84ce49f346018e
samdani1412 <bsse1412@iit.du.ac.bd>
2023-02-28 22:09:11 +0600

    accepted

---
+++ A_Petya_and_Origami.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<<" "<<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    ll n,k,sum;
+    cin>>n>>k;
+
+    if(((2*n)%k)==0) sum=(2*n)/k;
+    else sum=(2*n)/k +1;
+    
+    if(((5*n)%k)==0) sum=sum+((5*n)/k);
+    else sum=sum+1+((5*n)/k);
+
+    if(((8*n)%k)==0) sum=sum+((8*n)/k);
+    else sum=sum+1+((8*n)/k);
+
+    cout<<sum;
+
+    return 0;
+}
