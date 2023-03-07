6ae2601bbf5be140ef934c0bc01de2db3040512d
samdani91 <114305338+samdani91@users.noreply.github.com>
2023-03-07 19:12:19 +0600

    accepted

---
+++ A_Lucky_Division.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int arr[]={4,7,44,47,74,77,444,447,474,477,744,747,774,777};
+    int n;
+    cin>>n;
+    bool flag=false;
+
+    for(int i=0;i<14;i++){
+        if(n%arr[i]==0){
+            flag=true;
+            break;
+        }
+    }
+
+    if(flag) cout<<"YES";
+    else cout<<"NO";
+
+    return 0;
+}
