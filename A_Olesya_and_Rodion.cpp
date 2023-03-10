591821dfd2bf9a71bbb65a3872f61bb499450061
samdani91 <114305338+samdani91@users.noreply.github.com>
2023-03-10 11:14:36 +0600

    accepted

---
+++ A_Olesya_and_Rodion.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<<" "<<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int n,t;
+    cin>>n>>t;
+
+    if(n==1 && t!=10) cout<<t;
+    else if(n==1 && t==10) cout<<"-1";
+    else if(n>=2 && t==10){
+        for(int i=1;i<n;i++) cout<<"1";
+        cout<<"0";
+    }
+    else{
+        for(int i=1;i<=n;i++){
+            cout<<t;
+        }
+    }
+
+    return 0;
+}
