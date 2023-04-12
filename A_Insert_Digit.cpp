de39c749ff4c60dd125f2b76c22b439141b06a12
samdani91 <114305338+samdani91@users.noreply.github.com>
2023-04-12 16:44:21 +0600

    accepted

---
+++ A_Insert_Digit.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int t,n,k,i;
+    cin>>t;
+    while(t--){
+        cin>>n>>k;
+        string s;
+        cin>>s;
+
+        for(i=0;s[i];i++){
+            int temp=s[i]-48;
+            if(k<=temp){
+                cout<<s[i];
+            }else break;
+        }
+        cout<<k;
+        for(;s[i];i++) cout<<s[i];
+        cout<<endl;
+    }
+
+    return 0;
+}
