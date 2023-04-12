61f930c5c65417f152b22ce62984e7f73e82d9e1
samdani91 <114305338+samdani91@users.noreply.github.com>
2023-04-12 12:14:24 +0600

    accepted

---
+++ A_Download_More_RAM.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int t,n,k,temp,ram;
+    cin>>t;
+    while(t--){
+        cin>>n>>k;
+        int a[n],b[n];
+        for(int &ele:a) cin>>ele;
+        for(int &ele:b) cin>>ele;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]>a[j]){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+
+                    temp=b[i];
+                    b[i]=b[j];
+                    b[j]=temp;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(k>=a[i]){
+                k=k+b[i];
+            }
+        }
+
+        cout<<k<<endl;
+    }
+
+    return 0;
+}
