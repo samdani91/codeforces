f93cf353c6d951e6dedba6d4858d6e4dc1a6905e
samdani91 <114305338+samdani91@users.noreply.github.com>
2023-04-14 21:35:23 +0600

    accepted

---
+++ A_Cards.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    int countz=0,countn=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='z') countz++;
+        else if(s[i]=='n') countn++;
+    }
+    for(int i=0;i<countn;i++) cout<<1<<" ";
+    for(int i=0;i<countz;i++) cout<<0<<" ";
+
+    return 0;
+}
