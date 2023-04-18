a6565fda7b1b482320c2bce0c119bc3514754b1e
samdani91 <114305338+samdani91@users.noreply.github.com>
2023-04-18 18:04:06 +0600

    accepted

---
+++ A_Juggling_Letters.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int t,n,flag;
+    string s;
+    cin>>t;
+    while(t--){
+        flag=1;
+        cin>>n;
+        vector<int>count(26);
+        for(int j=1;j<=n;j++){
+            cin>>s;
+            for(int i=0;i<s.size();i++) count[s[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]%n!=0){
+                flag=0;
+                break;
+            }
+        }
+
+        if(flag==1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
