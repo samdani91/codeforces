79fa28946cb0713de2ef0b10ac1016fba97326d3
A. M Samdani Mozumder <114305338+samdani91@users.noreply.github.com>
2023-07-27 14:30:12 +0600

    accepted

---
+++ A_Fafa_and_his_Company.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int n,count=0;
+    cin>>n;
+
+    for(int i=1;i<=n/2;i++){
+        if(n%i==0) count++;
+    }
+
+    cout<<count;
+    
+    return 0;
+}
