c734fc272f76854bd877ee1b4559b2568d3c4273
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-13 23:15:19 +0600

    accepted

---
+++ B_The_Corridor_or_There_and_Back_Again.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int t,n,d,s,temp,k;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        k=INT_MAX;
+        while(n--){
+            cin>>d>>s;
+            if((s%2)==0){
+                temp=(s/2)-1;
+            }else{
+                temp=s/2;
+            }
+            k=min(d+temp,k);
+        }
+        cout<<k<<endl;
+    }
+
+    return 0;
+}
