c7ff12724e23f121249a889f9655cefb0f3a42b0
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-18 11:02:42 +0600

    accepted

---
+++ A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    ll n;
+    cin>>n;
+
+    if(n==0) cout<<1;
+    else if( n%2!=0 && n%4==1) cout<<8;
+    else if(n%2!=0 && n%4==3) cout<<2;
+    else if((n/2)%2!=0) cout<<4;
+    else if((n/2)%2==0) cout<<6;
+
+    return 0;
+}
