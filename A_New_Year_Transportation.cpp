0f25283205120d2a730c52da2016be7cf5d078ba
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-23 12:50:06 +0600

    accepted

---
+++ A_New_Year_Transportation.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+const int N=1e5+10;
+
+vector<int>graph[N];
+bool vis[N];
+
+void dfs(int s)
+{
+    vis[s]=true;
+    for(int v:graph[s]){
+        if(vis[v]) continue;
+
+        dfs(v);
+    }
+}
+int main()
+{
+
+    int n,t;
+    cin>>n>>t;
+    for(int i=1;i<n;i++){
+        int a;
+        cin>>a;
+        graph[i].push_back(i+a);
+    }
+
+    dfs(1);
+    if(vis[t]) cout<<"YES";
+    else cout<<"NO";
+
+    return 0;
+}
