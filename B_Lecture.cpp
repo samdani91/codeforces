bbc925b06d79ecabb4ed62c5bab182a4af0504fb
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-09-25 21:58:22 +0600

    accepted

---
+++ B_Lecture.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int n,m;
+    cin>>n>>m;
+
+    map<str,str>mp;
+
+    for(int i=1;i<=m;i++){
+        str f,s;
+        cin>>f>>s;
+        mp.insert({f,s});
+    }
+    loop{
+        str temp;
+        cin>>temp;
+        
+        if(temp.size()<=mp[temp].size()) cout<<temp<<" ";
+        else cout<<mp[temp]<<" ";
+    }
+
+    return 0;
+}
