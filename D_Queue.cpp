2eea412a7a17a5c9d416fb84a127983ba082d415
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-10-24 01:47:56 +0600

    accepted

---
+++ D_Queue.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    int n;cin>>n;
+    vector<int>v;
+    loop{
+        int x;cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+
+    ll sum=0;
+    int count=0;
+
+    for(int i=0;i<v.size();i++){
+        if(sum>v[i]) continue;
+        sum+=v[i];
+        count++;
+    }
+    cout<<count<<endl;
+
+    return 0;
+}
