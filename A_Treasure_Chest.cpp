27535929479b2492036698945c228021197bd879
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-11-23 16:11:19 +0600

    accepted

---
+++ A_Treasure_Chest.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int t;cin>>t;
+    while(t--){
+        ll x,y,k;
+        cin>>x>>y>>k;
+
+        ll ans=0;
+
+        if(x>=y) cout<<x<<endl;
+        else{
+            ll temp=y-x;
+            if(temp<=k) cout<<y<<endl;
+            else{
+                ll temp2=x+k;
+                temp2+=2*(y-temp2);
+                cout<<temp2<<endl;
+            }
+        }
+    }
+
+    return 0;
+}
