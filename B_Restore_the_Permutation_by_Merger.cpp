9298341681a3f3046684318f9236b2c24d7fb7f6
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-12-08 19:29:31 +0600

    accepted

---
+++ B_Restore_the_Permutation_by_Merger.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<< <<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        vll v;
+        for(int i=1;i<=2*n;i++){
+            int x;cin>>x;
+            auto it=find(v.begin(),v.end(),x);
+            if(it==v.end()) v.push_back(x);
+        }
+    
+        for(auto it:v) cout<<it<<" ";
+        cout<<"\n";
+    }
+
+    return 0;
+}
