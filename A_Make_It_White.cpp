f05edf262e32065b74b221722c8d0f07361e9e22
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2024-03-17 01:19:10 +0600

    accepted

---
+++ A_Make_It_White.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<<" "<<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+#define  endl                    "\n"
+
+const int N=1e5+10;
+const int M=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;cin>>n;
+        string s;cin>>s;
+        int st=0,e=0;
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='B'){
+                st=i+1;
+                break;
+            }
+        }
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='B'){
+                e=i+1;
+                break;
+            }
+        }
+
+        cout<<abs(st-e)+1<<endl;
+
+    }
+
+    return 0;
+}
---
+++ A_Vlad_And_The_Best_Of_Five.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef  long long int          ll;
+typedef  long double            ld;
+typedef  string                 str;
+typedef  vector<ll>             vll;
+typedef  vector<string>         vs;
+typedef  vector<pair<ll, ll>>   vpl;
+typedef  set<ll>                sll;
+typedef  map<ll,ll>             mll;
+typedef  pair<int,int>          pint;
+typedef  pair<ll,ll>            pll;
+double   pi = acos(-1.0);
+#define  debug(x)              cerr<<#x<<" "<<x<<endl;
+#define  loop                   for(ll i=1; i<=n; i++)
+#define  all(a)                 (a).begin(), (a).end()
+#define  min3(a,b,c)            min(a,min(b,c))
+#define  max3(a,b,c)            max(a,max(b,c))
+#define  min4(a,b,c,d)          min(a,min(b,min(c,d)));
+#define  max4(a,b,c,d)          max(a,max(b,max(c,d)));
+#define  endl                    "\n"
+
+const int N=1e5+10;
+const int M=1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+        string s;cin>>s;
+        map<char,int>mp;
+        for(int i=0;i<5;i++){
+            mp[s[i]]++;
+        }
+        if(mp['A']>mp['B']) cout<<"A\n";
+        else cout<<"B\n";
+
+    }
+
+    return 0;
+}
